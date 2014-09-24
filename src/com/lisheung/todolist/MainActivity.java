455189e0e9cd85d84d75876ae877f21b0a74d9c7
creativlimade <chris_lsy@hotmail.com>
2014-09-24 14:47:47 -0600

    Classes Moved.

--- src/com/lisheung/todolist/MainActivity.java
+++ src/com/lisheung/todolist/MainActivity.java
@@ -147,4 +147,45 @@ public class MainActivity extends Activity implements
 		}
 	}
 
+//=================================================================================//
+	
+	public class FragmentTodo extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+			// Inflate the layout for this fragment
+			return inflater.inflate(R.layout.fragment_todo, container,
+					false);
+		}
+	}
+	
+	public class FragmentArchive extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+			// Inflate the layout for this fragment
+			return inflater.inflate(R.layout.fragment_archive, container,
+					false);
+		}
+	}
+
+	public class FragmentEmail extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+			// Inflate the layout for this fragment
+			return inflater.inflate(R.layout.fragment_email, container,
+					false);
+		}
+	}
+	
+	public class FragmentSummary extends Fragment {
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+			// Inflate the layout for this fragment
+			return inflater.inflate(R.layout.fragment_summary, container,
+					false);
+		}
+	}
 }
